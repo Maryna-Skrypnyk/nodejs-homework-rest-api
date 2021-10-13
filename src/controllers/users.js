7c370e965e11f59e7ebb10fe8d901d2dd2d8e2a7
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2021-10-13 16:09:29 +0300

    Adds registration - signup

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,33 @@
 const Users = require("../../repository/users");
+const { HttpCode } = require("../../config/constants");
 
-const signup = async (req, res, next) => {};
+const signup = async (req, res, next) => {
+  const { email, password, subscription } = req.body;
+  const user = await Users.findByEmail(email);
+  if (user) {
+    return res.status(HttpCode.CONFLICT).json({
+      status: "error",
+      code: HttpCode.CONFLICT,
+      message: "Email in use",
+    });
+  }
+
+  try {
+    const newUser = await Users.create({ email, password, subscription });
+    return res.status(HttpCode.CREATED).json({
+      status: "success",
+      code: HttpCode.CREATED,
+      data: {
+        id: newUser.id,
+        email: newUser.email,
+        password: newUser.password,
+        subscription: newUser.subscription,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const login = async (req, res, next) => {};
 
