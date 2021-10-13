47ef6091218d98cb48c1b7e08325fb485d0d70c5
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2021-10-13 16:53:51 +0300

    Adds login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
 const Users = require("../../repository/users");
 const { HttpCode } = require("../../config/constants");
+require("dotenv").config();
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const signup = async (req, res, next) => {
   const { email, password, subscription } = req.body;
@@ -29,7 +32,30 @@ const signup = async (req, res, next) => {
   }
 };
 
-const login = async (req, res, next) => {};
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await Users.findByEmail(email);
+  const isValidPassword = await user.isValidPassword(password);
+
+  if (!user || !isValidPassword) {
+    return res.status(HttpCode.UNAUTHORIZED).json({
+      status: "error",
+      code: HttpCode.UNAUTHORIZED,
+      message: "Email or password is wrong",
+    });
+  }
+
+  const id = user._id;
+  const payload = { id };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  await Users.updateToken(id, token);
+
+  return res.status(HttpCode.OK).json({
+    status: "success",
+    code: HttpCode.OK,
+    data: { token },
+  });
+};
 
 const logout = async (req, res, next) => {};
 
